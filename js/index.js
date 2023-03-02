7d8d18be06722ba8b9912ae9b6e1fb5b2ea43641
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-03-02 12:38:15 +0100

    broke the app

--- js/index.js
+++ js/index.js
@@ -1,63 +1,55 @@
 let quoteContainer = document.getElementById("quote");
-let author= document.getElementById("author")
+let author = document.getElementById("author");
 let button = document.getElementById("btn");
+let previousQuotes = [];
 
-//   quoteContainer.innerText = "Loading...";
-// fetch("https://api.quotable.io/random")
-//   .then((response) => response.json())
-//   .then((fetchResult) => (quoteContainer.innerText = '" ' + fetchResult.content + ' "'));
 
-// let quoteContainer = document.getElementById("quote");
-// // // let container = document.querySelector("quote");
 
-// getQuote = () => {
-//   fetch("https://api.quotable.io/random")
-//     .then((response) => response.json())
-//     .then((fetchResult) => (quoteContainer.innerText = '" ' + fetchResult.content + ' "'));
-// };
 
-// button.addEventListener("click", getQuote());
+let overlayWindow = document.getElementById("overlay");
+let openOverlayButton = document.getElementById("openOverlayBtn");
+let closeOverlayButton = document.getElementById("closeOverlayBtn");
+let overlayContents = document.getElementById("overlayContents");
+    //   quoteContainer.innerText = "Loading...";
+    // fetch("https://api.quotable.io/random")
+    //   .then((response) => response.json())
+    //   .then((fetchResult) => (quoteContainer.innerText = '" ' + fetchResult.content + ' "'));
 
-// const quoteContainer = document.getElementById("quote");
-//   quoteContainer.innerText = "Loading...";
-// fetch("https://api.quotable.io/random")
-//   .then((response) => response.json())
-//   .then((fetchResult) => (quoteContainer.innerText = '" ' + fetchResult.content + ' "'));
+    // let quoteContainer = document.getElementById("quote");
+    // // // let container = document.querySelector("quote");
+
+    // getQuote = () => {
+    //   fetch("https://api.quotable.io/random")
+    //     .then((response) => response.json())
+    //     .then((fetchResult) => (quoteContainer.innerText = '" ' + fetchResult.content + ' "'));
+    // };
+
+    // button.addEventListener("click", getQuote());
+
+    // const quoteContainer = document.getElementById("quote");
+    //   quoteContainer.innerText = "Loading...";
+    // fetch("https://api.quotable.io/random")
+    //   .then((response) => response.json())
+    //   .then((fetchResult) => (quoteContainer.innerText = '" ' + fetchResult.content + ' "'));
+
+
 
 quoteContainer.innerText = "Loading...";
+
+
 fetch("https://api.quotable.io/random")
   .then((response) => {
     if (!response.ok) {
-      throw new Error("Failed to fetch quote");
+      throw new Error("Failed to fetch quote - check your internet connection");
     }
     return response.json();
   })
   .then((fetchResult) => {
     quoteContainer.innerText = `"${fetchResult.content}"`;
     author.innerText = `- ${fetchResult.author}`;
+    addNewPreviousQuote(fetchResult);
+      // previousQuotes.push(`"${fetchResult.content}" - ${fetchResult.author}`);
+
   })
-  .catch((error) => {
-    quoteContainer.innerText = "Failed to fetch quote";
-    console.error(error);
-  });
 
-const getQuote = () => {
-  quoteContainer.innerText = "Loading...";
-  fetch("https://api.quotable.io/random")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to fetch quote");
-      }
-      return response.json();
-    })
-    .then((fetchResult) => {
-      quoteContainer.innerText = `"${fetchResult.content}"`;
-      author.innerText = `- ${fetchResult.author}`;
-    })
-    .catch((error) => {
-      quoteContainer.innerText = "Failed to fetch quote";
-      console.error(error);
-    });
-};
 
-button.addEventListener("click", getQuote);
